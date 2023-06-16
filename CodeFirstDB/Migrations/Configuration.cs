0387b3248e8e3b77c5ed15e2ec797186d8f160ec
Kyle Padayachee <kyle.padayachee@gamesglobal.com>
2023-06-16 11:55:45 +0200

    basic entity framework migration project

---
+++ CodeFirstDB/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstDB.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyApp.Program.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyApp.Program.BlogDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CodeFirstDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DatePublished { get; set; } 
+    }
+    internal class Program
+    {
+
+        public class BlogDbContext: DbContext
+        {
+            public DbSet<Post> Posts { get; set; }  
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
